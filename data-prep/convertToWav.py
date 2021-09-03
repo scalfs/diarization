493f41bd83bf377abc63a754d47cc7dc24f5717a
Vitor Escalfoni <vitor.hme@gmail.com>
2021-09-03 17:41:20 -0300

    convert voxceleb2-test script

--- data-prep/convertToWav.py
+++ data-prep/convertToWav.py
@@ -1,18 +1,17 @@
 from tqdm import tqdm
-import os
 import glob
 import subprocess
 
 
-files = glob.glob('/home/jovyan/work/datasets/voxceleb-2/dev/aac/*/*/*.m4a')
+files = glob.glob('/home/jovyan/work/datasets/voxceleb-2/test/aac/*/*/*.m4a')
 files.sort()
 
 print('Converting files from AAC to WAV')
 for fname in tqdm(files):
     outfile = fname.replace('.m4a', '.wav')
-    if os.path.isfile(outfile):
-        print(outfile)
-        continue
+    # if os.path.isfile(outfile):
+    #     print(outfile)
+    #     continue
 
     out = subprocess.call(
         'ffmpeg -y -i %s -ac 1 -vn -acodec pcm_s16le -ar 16000 %s >/dev/null 2>/dev/null' % (fname, outfile), shell=True)
