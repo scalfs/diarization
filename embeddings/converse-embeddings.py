0d0554f93e7ad624db5ad273403f864568f70a44
Vitor Escalfoni <vitor.hme@gmail.com>
2021-09-22 11:28:46 -0300

    fixed segment intervals

--- embeddings/converse-embeddings.py
+++ embeddings/converse-embeddings.py
@@ -26,20 +26,21 @@ logging.basicConfig(
     filename=log_file,
     level=logging.DEBUG,
     format="%(asctime)s:%(levelname)s:%(message)s"
-    )
+)
 print(f'Log path: {log_file}')
 
 data_path = '/app/datasets/voxconverse/test/wav'
 rttm_path = '/app/datasets/voxconverse/test/rttm'
-save_dir_path = '/app/voxsrc21-dia/embeddings/sequences/voxconverse-test'
+save_dir_path = '/app/voxsrc21-dia/embeddings/sequences'
 config.model_path = '/app/voxsrc21-dia/models/model.ckpt-46'
 os.makedirs(save_dir_path, exist_ok=True)
+
 
 def concat_segs(times, segs):
     # Concatenate continuous voiced segments
     # with segment time information (onset and offset)
     concat_seg = []
-    concat_times=[]
+    concat_times = []
     seg_concat = segs[0]
     seg_onset = times[0][0]
     for i in range(0, len(times)-1):
@@ -64,32 +65,42 @@ def concat_segs(times, segs):
 
     return concat_seg, concat_times
 
+
 def get_STFTs(segs, time_segs):
-    #Get 240ms STFT windows with 50% overlap, in pairs
+    # Get 240ms STFT windows with 50% overlap
     sr = config.sr
     STFT_windows = []
     time_windows = []
     for i, seg in enumerate(segs):
-        S = librosa.core.stft(y=seg, n_fft=config.nfft, win_length=int(config.window * sr), hop_length=int(config.hop * sr))
+        S = librosa.core.stft(y=seg, n_fft=config.nfft, win_length=int(
+            config.window * sr), hop_length=int(config.hop * sr))
         S = np.abs(S) ** 2
         mel_basis = librosa.filters.mel(sr=sr, n_fft=config.nfft, n_mels=40)
         # log mel spectrogram of utterances
         S = np.log10(np.dot(mel_basis, S) + 1e-6)
 
-        # S.shape[1] ~= math.ceil((time_segs[i][1] - time_segs[i][0])*100+1)
+        STFT_couple = []
         segment_time_onset = time_segs[i][0]
-        for j in range(0, S.shape[1], int(.24/config.hop)): # 0.24 / 0.01 = 24.0
-            # if hop != 0.01, we can't use 12, 24, 36 frames (they stop making sense)
-            # 36 frames are related to .36s of the audio
-            if j + 36 < S.shape[1]:
-                # in order to fit on the expected shape of the embedding network we double the window
-                STFT_windows.append([S[:, j:j+24], S[:, j+12:j+36]])
-                # returns the time intervals for each STFT window
-                window_onset = segment_time_onset + 0.01*j
-                time_windows.extend([[window_onset, window_onset+0.24], [window_onset+0.12, window_onset+0.36]])
+        for j in range(0, S.shape[1], int(.12/config.hop)):  # 0.24 / 0.01 = 24.0
+            # if hop != 0.01, we can't use 12 or 24 frames (they stop making sense)
+            if j + 24 < S.shape[1]:
+                if len(STFT_couple) < 2:
+                    # we save the stft in couples because of the expected entry of the lstm embedding network
+                    # it was trained like this, so its expected by the pre-trained model and weights
+                    STFT_couple.append(S[:, j:j+24])
+                else:
+                    STFT_windows.append(STFT_couple)
+                    STFT_couple = [S[:, j:j+24]]
+                    # returns the time intervals for each STFT window
+                    window_onset = segment_time_onset + 0.01*j
+                    # the time windows doesn't have to be save in couples
+                    # after processing, the embeddings and time_windows will have the same length
+                    time_windows.extend(
+                        [[window_onset-0.12, window_onset+0.12], [window_onset, window_onset+0.24]])
             else:
                 break
     return np.array(STFT_windows), np.array(time_windows)
+
 
 def align_embeddings(embeddings, intervals):
     partitions = []
@@ -100,44 +111,61 @@ def align_embeddings(embeddings, intervals):
         if (i*.12)+.24 < j*.401:
             end = end + 1
         else:
-            partitions.append((start,end))
+            partitions.append((start, end))
             start = end
             end = end + 1
             j += 1
     else:
-        partitions.append((start,end))
+        partitions.append((start, end))
 
-    avg_embeddings = np.zeros((len(partitions),256))
+    avg_embeddings = np.zeros((len(partitions), 256))
     segment_intervals = []
+    use_first_partition_onset = True
     for i, partition in enumerate(partitions):
-        avg_embeddings[i] = np.average(embeddings[partition[0]:partition[1]],axis=0)
+        avg_embeddings[i] = np.average(
+            embeddings[partition[0]:partition[1]], axis=0)
 
         partition_interval = intervals[partition[0]:partition[1]]
-        interval_onset = partition_interval[0][0]   #start of first partition
-        last_partition_idx = -2 if len(partition_interval) > 1 else -1
-        interval_offset = partition_interval[last_partition_idx][1] #end of last partition
+        # start of first partition (the second really, because of the previous window step)
+        partition_onset_idx = 0 if use_first_partition_onset else 1
+        interval_onset = partition_interval[partition_onset_idx][0]
+        # end of last partition
+        use_first_partition_onset = True if partition_interval[-1][1] - \
+            interval_onset > 0.4 else False
+        partition_offset_idx = -2 if use_first_partition_onset else -1
+        interval_offset = partition_interval[partition_offset_idx][1]
+        # segment interval for the partition
         segment_intervals.append([interval_onset, interval_offset])
     return avg_embeddings, np.array(segment_intervals)
 
+
 def getOnsets(turn):
     return turn.onset
+
 
 def main():
     # Data prep
     # I'm saving only 2 embeddings i.e. first and last tisv_frames for given interval in an audio. So each .npy
     # embedding file will have a shape of (2, 256)
     tf.reset_default_graph()
-    batch_size = 2 # Fixing to 2 since we take 2 for each interval #utter_batch.shape[1]
-    verif = tf.placeholder(shape=[None, batch_size, 40], dtype=tf.float32)  # verification batch (time x batch x n_mel)
-    batch = tf.concat([verif,], axis=1)
+    # Fixing to 2 since we take 2 for each interval #utter_batch.shape[1]
+    batch_size = 2
+    # verification batch (time x batch x n_mel)
+    verif = tf.placeholder(shape=[None, batch_size, 40], dtype=tf.float32)
+    batch = tf.concat([verif, ], axis=1)
     # embedding lstm (3-layer default)
     with tf.variable_scope("lstm"):
-        lstm_cells = [tf.contrib.rnn.LSTMCell(num_units=config.hidden, num_proj=config.proj) for i in range(config.num_layer)]
-        lstm = tf.contrib.rnn.MultiRNNCell(lstm_cells)    # make lstm op and variables
-        outputs, _ = tf.nn.dynamic_rnn(cell=lstm, inputs=batch, dtype=tf.float32, time_major=True)   # for TI-VS must use dynamic rnn
-        embedded = outputs[-1]                            # the last ouput is the embedded d-vector
+        lstm_cells = [tf.contrib.rnn.LSTMCell(
+            num_units=config.hidden, num_proj=config.proj) for i in range(config.num_layer)]
+        lstm = tf.contrib.rnn.MultiRNNCell(
+            lstm_cells)    # make lstm op and variables
+        # for TI-VS must use dynamic rnn
+        outputs, _ = tf.nn.dynamic_rnn(
+            cell=lstm, inputs=batch, dtype=tf.float32, time_major=True)
+        # the last ouput is the embedded d-vector
+        embedded = outputs[-1]
         embedded = normalize(embedded)                    # normalize
-    config_tensorflow = tf.ConfigProto(device_count = {'GPU': 0})
+    config_tensorflow = tf.ConfigProto(device_count={'GPU': 0})
     saver = tf.train.Saver(var_list=tf.global_variables())
 
     all_unique_extensions = []
@@ -146,8 +174,8 @@ def main():
     rttm_files = defaultdict(list)
 
     for audio_file in os.listdir(data_path):
-        if audio_file.startswith('.'): #hidden folders
-            continue;
+        if audio_file.startswith('.'):  # hidden folders
+            continue
         audio_id = os.path.splitext(audio_file)[0]
         extension = os.path.splitext(audio_file)[1]
         all_unique_extensions.append(extension)
@@ -156,7 +184,8 @@ def main():
             audio_files[audio_id] = os.path.join(data_path, audio_file)
             rttm_files[audio_id] = os.path.join(rttm_path, audio_id + '.rttm')
         else:
-            logging.info(f'Wrong file type in {os.path.join(data_path, audio_file)}')
+            logging.info(
+                f'Wrong file type in {os.path.join(data_path, audio_file)}')
 
     audio_quantity = len(audio_files)
     logging.info(f'Unique file extensions: {set(all_unique_extensions)}')
@@ -184,15 +213,17 @@ def main():
             STFT_windows, time_windows = get_STFTs(concat_seg, concat_times)
             # print(len(STFT_windows), STFT_windows[0].shape)
 
-            embeddings = np.array([]).reshape(0,256)
+            embeddings = np.array([]).reshape(0, 256)
             for STFT_window in STFT_windows:
-                STFT_batch = np.transpose(STFT_window, axes=(2,0,1))
+                STFT_batch = np.transpose(STFT_window, axes=(2, 0, 1))
                 # print(STFT_batch.shape) (24, 2, 40) (240ms window * batch 2 * mels 40)
-                embeddings_batch = sess.run(embedded, feed_dict={verif:STFT_batch})
+                embeddings_batch = sess.run(
+                    embedded, feed_dict={verif: STFT_batch})
                 embeddings = np.concatenate((embeddings, embeddings_batch))
 
             # Turn window-level embeddings to segment-level (400ms)
-            aligned_embeddings, segment_intervals = align_embeddings(embeddings, time_windows)
+            aligned_embeddings, segment_intervals = align_embeddings(
+                embeddings, time_windows)
 
     #         # Comparar com o turns retornado pelo load_rttm para montar o train_cluster_ids
     #         turns, _, _ = load_rttm(rttm_files.get(audio_id)[0])
@@ -205,16 +236,20 @@ def main():
             audio_count += 1
 
             if (audio_count == audio_quantity or audio_count % 20 == 0):
-                train_sequences_path = os.path.join(save_dir_path, f'voxcon-test-train-sequences.npy')
+                train_sequences_path = os.path.join(
+                    save_dir_path, f'voxcon-test-sequences.npy')
                 np.save(train_sequences_path, train_sequences)
 
-                intervals_path = os.path.join(save_dir_path, f'voxcon-test-intervals.npy')
+                intervals_path = os.path.join(
+                    save_dir_path, f'voxcon-test-intervals.npy')
                 np.save(intervals_path, sequence_intervals)
 
     #             train_cluster_ids_path = os.path.join(save_dir_path, f'voxcon-dev-train-cluster-ids.npy')
     #             train_cluster_ids = np.asarray(train_cluster_ids)
     #             np.save(train_cluster_ids_path, train_cluster_ids)
-                logging.info(f'saved train sequence {audio_count}/{audio_quantity}')
+                logging.info(
+                    f'saved test sequence {audio_count}/{audio_quantity}')
+
 
 if __name__ == "__main__":
     """
