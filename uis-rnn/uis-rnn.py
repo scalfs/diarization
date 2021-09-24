b7249d122b0bbe5445a79dcc00d829e417ddea19
scalfs <vitor.hme@gmail.com>
2021-09-24 02:49:45 -0300

    reduced train iteration to converging point

--- uis-rnn/uis-rnn.py
+++ uis-rnn/uis-rnn.py
@@ -9,8 +9,6 @@ def diarization_experiment(model_args, training_args, inference_args):
     train_sequence = np.load('/app/fixed-voxcon-dev-sequences.npy', allow_pickle=True)
     train_cluster_id = np.load('/app/voxsrc21-dia/embeddings/sequences/voxcon-dev-cluster-ids.npy', allow_pickle=True)
     
-    model = uisrnn.UISRNN(model_args)
-    
     # How many elements each 
     # list should have 
     n = 53
@@ -19,6 +17,9 @@ def diarization_experiment(model_args, training_args, inference_args):
     split_train_sequence = [train_sequence[i:i + n] for i in range(0, len(train_sequence), n)]
     split_train_cluster_id = [train_cluster_id[i:i + n] for i in range(0, len(train_cluster_id), n)]
     
+    training_args.train_iteration = 210
+    model = uisrnn.UISRNN(model_args)
+    
     for sequence, cluster_id in zip(split_train_sequence, split_train_cluster_id):
         concatenated_train_sequence = np.concatenate(sequence)
         concatenated_train_cluster_id = np.concatenate(cluster_id)
@@ -26,9 +27,9 @@ def diarization_experiment(model_args, training_args, inference_args):
         # Training
         model.fit(concatenated_train_sequence, concatenated_train_cluster_id, training_args)
         print('foi')
+        model.save(SAVED_MODEL_NAME)
     
     
-    model.save(SAVED_MODEL_NAME)
     
     
 def main():
