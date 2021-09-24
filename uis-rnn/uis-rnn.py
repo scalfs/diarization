26d896d0dc6dbd93394b07a3976c887e4c64ad0f
scalfs <vitor.hme@gmail.com>
2021-09-24 00:36:16 -0300

    concatenating lists

--- uis-rnn/uis-rnn.py
+++ uis-rnn/uis-rnn.py
@@ -9,14 +9,16 @@ def diarization_experiment(model_args, training_args, inference_args):
     train_sequence = np.load('/app/fixed-voxcon-dev-sequences.npy', allow_pickle=True)
     train_cluster_id = np.load('/app/voxsrc21-dia/embeddings/sequences/voxcon-dev-cluster-ids.npy', allow_pickle=True)
     
+    concatenated_train_sequence = np.concatenate(train_sequence)
+    concatenated_train_cluster_id = np.concatenate(train_cluster_id)
+    
     model = uisrnn.UISRNN(model_args)
     # Training.
     # If we have saved a mode previously, we can also skip training by
     # calling：
     # model.load(SAVED_MODEL_NAME)
-    model.fit(train_sequence, train_cluster_id, training_args)
+    model.fit(concatenated_train_sequence, concatenated_train_cluster_id, training_args)
     model.save(SAVED_MODEL_NAME)
-    
     
     
 def main():
