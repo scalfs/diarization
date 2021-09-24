d6dff3374f6f12c15e9eedad37679e538ad47ff0
scalfs <vitor.hme@gmail.com>
2021-09-23 23:19:46 -0300

    voxcon dev uisrnn training

---
+++ uis-rnn/uis-rnn.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+import uisrnn
+
+SAVED_MODEL_NAME = 'voxcon_dev_model.uisrnn'
+
+def diarization_experiment(model_args, training_args, inference_args):
+
+    train_sequence = np.load('./data/toy_training_data.npz', allow_pickle=True)
+    train_cluster_id = np.load('./data/toy_testing_data.npz', allow_pickle=True)
+    
+    # Training.
+    # If we have saved a mode previously, we can also skip training by
+    # calling：
+    # model.load(SAVED_MODEL_NAME)
+    model.fit(train_sequence, train_cluster_id, training_args)
+    model.save(SAVED_MODEL_NAME)
+    
+    
+    
+def main():
+    """The main function."""
+    model_args, training_args, inference_args = uisrnn.parse_arguments()
+    print(
+    diarization_experiment(model_args, training_args, inference_args)
+
+
+if __name__ == '__main__':
+    main()
